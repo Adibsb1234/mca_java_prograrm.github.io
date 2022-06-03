7971864226fff2374c3f5d604b84f477e8ced165
Aditya Agarwal <adityatd189@gmail.com>
2022-06-03 23:07:27 +0530

    Update Queue using Two Stacks.java

--- Queue using Two Stacks.java
+++ Queue using Two Stacks.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
             Scanner in=new Scanner(System.in);
             int n=in.nextInt();
             int arr[]=new int[n];
