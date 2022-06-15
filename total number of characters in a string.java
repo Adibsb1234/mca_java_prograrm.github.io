6eeeebf2a80264da67e57e0a4580bc7c8a747298
Aditya Agarwal <adityatd189@gmail.com>
2022-06-15 23:01:22 +0530

    Update total number of characters in a string.java

--- total number of characters in a string.java
+++ total number of characters in a string.java
@@ -4,13 +4,11 @@ public class CountCharacter
         String string = "The best of both worlds";    
         int count = 0;    
             
-        //Counts each character except space    
         for(int i = 0; i < string.length(); i++) {    
             if(string.charAt(i) != ' ')    
                 count++;    
         }    
             
-        //Displays the total number of characters present in the given string    
         System.out.println("Total number of characters in a string: " + count);    
     }    
 }     
