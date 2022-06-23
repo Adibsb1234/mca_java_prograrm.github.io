37cff76b3549910421938b88e99f4f07282b3f7a
Aditya Agarwal <adityatd189@gmail.com>
2022-06-23 22:29:56 +0530

    Update Components in a graph.java

--- Components in a graph.java
+++ Components in a graph.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] parent = new int[2 * n + 1];
@@ -28,8 +27,6 @@ public class Solution {
                 parent[root_g] = root_b;
                 count[root_b] += count[root_g];               
             }
-            //System.out.println(g + "::" + b);
-            //System.out.println(parent[g] + "->" + parent[b]);
         }
         int min = 2 * n + 1;
         int max = 2;
@@ -38,7 +35,6 @@ public class Solution {
             if (count[i] == 1) continue;
             min = Math.min(min, count[i]);
             max = Math.max(max, count[i]);
-            //System.out.println(i + ":" + count[i]);
         }
         System.out.println(min + " " + max);
     }
