2106be3b9f2bc7d9f88f7a72c08fd13e01c9ab5f
Aditya Agarwal <adityatd189@gmail.com>
2022-07-22 20:37:36 +0530

    Update number to word.java

--- number to word.java
+++ number to word.java
@@ -1,14 +1,10 @@
 class NumberToWordExample1   
 {  
-//user-defined static method that converts a number into words  
 static void numberToWords(char num[])  
 {  
-//determines the number of digits in the given number  
 int len = num.length;  
-//checks the given number has number or not  
 if (len == 0)   
 {  
-//if the given number is empty prints the following statement     
 System.out.println("The string is empty.");  
 return;  
 }  
