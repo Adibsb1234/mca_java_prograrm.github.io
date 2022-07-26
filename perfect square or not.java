982b7a904edd6cf56d9ac0ad25f7a9b45af2c4e4
Aditya Agarwal <adityatd189@gmail.com>
2022-07-26 20:54:26 +0530

    Update perfect square or not.java

--- perfect square or not.java
+++ perfect square or not.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;  
 public class CheckPerfectSquareExample1  
 {   
-//user-defined method that checks the number is perfect square or not  
 static boolean checkPerfectSquare(double number)    
 {   
-//calculating the square root of the given number  
 double sqrt=Math.sqrt(number);   
 //finds the floor value of the square root and comparing it with zero  
 return ((sqrt - Math.floor(sqrt)) == 0);   
