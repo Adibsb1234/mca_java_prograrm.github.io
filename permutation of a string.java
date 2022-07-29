928b96ab70f23aeea8fd1b55889f9c80851c080b
Aditya Agarwal <adityatd189@gmail.com>
2022-07-29 22:47:00 +0530

    Update permutation of a string.java

--- permutation of a string.java
+++ permutation of a string.java
@@ -18,18 +18,14 @@ public class PermuteString {
         
     public static void generatePermutation(String str, int start, int end)    
     {    
-        //Prints the permutations    
         if (start == end-1)    
             System.out.println(str);    
         else    
         {    
             for (int i = start; i < end; i++)    
             {    
-                //Swapping the string by fixing a character    
                 str = swapString(str,start,i);    
-                //Recursively calling function generatePermutation() for rest of the characters     
                 generatePermutation(str,start+1,end);    
-                //Backtracking and swapping the characters again.    
                 str = swapString(str,start,i);    
             }    
         }    
